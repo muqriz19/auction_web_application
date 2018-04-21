87efdd3429e57e98c3febe252c262802ec601aea
muqriz <muqriz19othman@gmail.com>
2018-04-21 13:35:51 +0800

    fixed few stuff

--- login.php
+++ login.php
@@ -29,7 +29,25 @@
         $password = mysqli_real_escape_string($conn,$password);
 
         //check whether user exist in db or not
-        $query = "SELECT * FROM ";
+        $query = "SELECT * FROM  user WHERE '$username' and password='".md5($password)."'";
+
+        $result = mysqli_query($conn,$query) or die(mysql_error());
+
+        $rows = mysqli_num_rows($result);
+        
+        if($rows == 1) {
+            $_SESSION['username'] = $username;
+            // Redirect user to index.php
+            header("Location: index.php");
+            
+        } else {
+
+            echo "<div class='form'>
+            <h3>Username/password is incorrect.</h3>
+            <br/>Click here to <a href='login.php'>Login</a></div>";
+
+        }
+
 
     }
 
